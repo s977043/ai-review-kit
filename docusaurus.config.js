3b3fe7115230aa02799fc353705429a31238545f
Masatake Komine <minewo@DESKTOP-A1US1UP.localdomain>
2025-12-03 01:01:49 +0900

    Fix docs links to new docs base path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ module.exports = {
   themeConfig: {
     navbar: {
       title: 'River Reviewer',
-      items: [{ to: '/explanation/intro', label: 'Docs', position: 'left' }],
+      items: [{ to: '/docs/explanation/intro', label: 'Docs', position: 'left' }],
     },
     footer: {
       style: 'dark',
